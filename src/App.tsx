f2feb4909694f984febf84e287b8fe183484b542
mrsepdiana <firdasepdiana@gmail.com>
2021-04-15 15:59:45 +0700

    initial configuration for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Page } from "teemsly";
+import { PrivateRouteReactContext as RouteContext } from "teemsly-authorization-beta";
 import { DetectNetwork as detectNetwork } from "./utils";
+import { forbiddenUrl, loginUrl } from "./config/app";
+import { AppRouter, routers } from "./routers";
+
+import "teemsly/dist/styles/teemsly-default.css";
 
 function App() {
   /** Detect network connection  */
   detectNetwork();
 
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <RouteContext.Provider
+        value={{
+          authorities: ["ADMIN"],
+          loginUrl: loginUrl,
+          unAuthorizedUrl: forbiddenUrl,
+          isAuthenticated: false,
+        }}
+      >
+        <Page>
+          <AppRouter routers={routers} />
+        </Page>
+      </RouteContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
