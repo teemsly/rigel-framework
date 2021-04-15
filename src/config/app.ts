d1cd4bb41cdce835cc52c4eefb1e3dde46a6ddb3
mrsepdiana <firdasepdiana@gmail.com>
2021-04-15 09:04:33 +0700

    add app configuration

---
+++ src/config/app.ts
@@ -0,0 +1,18 @@
+/**
+ * Api Server 
+ * Specified host or server path to the APIs.
+ */
+export const apiServer = 'https://localhost:3030'
+
+/**
+ * API Timeout
+ * This timeout time when calling APIs, the time set to milisecond
+ * this default to 1200 (2 minutes).
+ */
+export const apiTimeout = 1200
+
+/** 
+ * The authentication token name
+ * This token name that will store to localStorage, sessionStorage, or Cookies HttpOnly
+ */
+export const authTokenName = 'auth_token'
