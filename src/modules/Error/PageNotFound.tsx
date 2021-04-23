a4e5fb6caaaa9866aebca9040f2b44e00359f4e1
mrsepdiana <firdasepdiana@gmail.com>
2021-04-23 19:47:26 +0700

    add module error page

---
+++ src/modules/Error/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div style={{ padding: "40px 60px" }}>
+      <h3>Error</h3>
+      <pre>No route found</pre>
+    </div>
+  );
+};
+
+export default PageNotFound;
