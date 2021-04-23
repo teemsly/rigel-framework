b25a86a71457aa49782bd3377b5309e6d2856aca
mrsepdiana <firdasepdiana@gmail.com>
2021-04-23 19:47:45 +0700

    initial routers

--- src/routers/routerName.tsx
+++ src/routers/routerName.tsx
@@ -1,5 +1,8 @@
 const routerName = {
   base: "/",
+  login: "/login",
+  signup: "/signup",
+  forgotPassword: "/forgot-password",
 };
 
 export default routerName;
--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,13 +1,21 @@
-import { Home } from "../modules";
+import { lazy } from "react";
 import routerName from "./routerName";
 
 const routers = [
   {
     path: routerName.base,
-    component: Home,
+    component: lazy(() => import("../modules/Home")),
     // auth: true,
     // hasAuthorities: [],
     exact: true,
+  },
+  {
+    path: routerName.login,
+    component: lazy(() => import("../modules/Login")),
+  },
+  {
+    path: "*",
+    component: lazy(() => import("../modules/Error/PageNotFound")),
   },
 ];
 
