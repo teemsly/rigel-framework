96d9044c52c5276c3e598c38f3374b677c62877d
mrsepdiana <firdasepdiana@gmail.com>
2021-04-27 15:10:34 +0700

    use setEnv for set configuration value

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,42 +1,44 @@
+import {setEnv} from '../utils';
+
 /**
  * Api Server 
  * Specified host or server path to the APIs.
  */
-export const apiServer = 'https://localhost:3030'
+export const apiServer = setEnv("https://localhost:3030");
 
 /**
  * API Timeout
  * This timeout time when calling APIs, the time set to milisecond
  * this default to 120000 (2 minutes).
  */
-export const apiTimeout = 120000
+export const apiTimeout = setEnv(120000);
 
 /** 
  * The authentication token name
  * This token name that will store to localStorage, sessionStorage, or Cookies HttpOnly
  */
-export const authTokenName = 'auth_token'
+export const authTokenName = setEnv("auth_token");
 
 /**
  * loginUrl
  * The specific login url path.
  */
-export const loginUrl = '/login'
+export const loginUrl = setEnv("/login");
 
 /**
  * forbiddenUrl
  * The specific forbidden url path for forbidden page. 
  */
-export const forbiddenUrl = '/forbidden'
+export const forbiddenUrl = setEnv("/forbidden");
 
 /**
  * Default Language
  * This default language callback
  */
-export const defaultLang = 'en-US'
+export const defaultLang = setEnv("en-US");
 
 /**
  * i18n Debug Mode
  * Internationalization debug mode
  */
-export const i18nDebugMode = true
+export const i18nDebugMode = setEnv(false, true, { productionOnly: true });
