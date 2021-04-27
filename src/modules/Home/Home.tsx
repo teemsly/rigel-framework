92bf86f661ea2d931c4d9df32121aff50a495476
mrsepdiana <firdasepdiana@gmail.com>
2021-04-27 23:30:50 +0700

    implement app layout and app top navigation

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { AppLayout, AppTopNavigation } from "../../shared";
 
 const Home = () => {
   const { t } = useTranslation("home");
 
   return (
-    <div style={{ textAlign: "center", padding: "40px 0", width: "100%" }}>
-      <h2>{t("title")}</h2>
-    </div>
+    <AppLayout>
+      <AppTopNavigation title="Home" />
+
+      <div style={{ textAlign: "center", padding: "40px 0", width: "100%" }}>
+        <h2>{t("title")}</h2>
+      </div>
+    </AppLayout>
   );
 };
 
